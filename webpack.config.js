727599530dc57f2b1415866dade672fe1db1165d
Marcin Wosinek <marcin.wosinek@gmail.com>
2021-04-21 10:40:18 +0100

    fix performance

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require("path"),
   HtmlWebpackPlugin = require("html-webpack-plugin"),
   pages = ["a", "b", "c", "d", "e"];
 
-module.exports = pages.map((page) => {
-  return {
-    entry: {
-      [page]: `./src/${page}.js`,
+module.exports = {
+  entry: pages.reduce(
+    (value, page) => Object.assign(value, { [page]: `./src/${page}.js` }),
+    {}
+  ),
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-    output: {
-      filename: "[name].js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    optimization: {
-      splitChunks: {
-        chunks: "all",
-      },
-    },
-    plugins: [
+  },
+  plugins: pages.map(
+    (page) =>
       new HtmlWebpackPlugin({
         inject: true,
         template: `./${page}.html`,
         filename: `${page}.html`,
         chunks: [page],
-      }),
-    ],
-  };
-});
+      })
+  ),
+};
