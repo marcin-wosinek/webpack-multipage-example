22514571d9a6eee9c5d1890b2996b61e35aa3f8b
Marcin Wosinek <marcin.wosinek@gmail.com>
2021-04-21 10:19:27 +0100

    show risk of using array of configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,27 @@
 const path = require("path"),
   HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: {
-    a: "./src/a.js",
-    b: "./src/b.js",
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+module.exports = ["a", "b"].map((page) => {
+  return {
+    entry: {
+      [page]: `./src/${page}.js`,
     },
-  },
-  plugins: [].concat(
-    ["a", "b"].map(
-      (page) =>
-        new HtmlWebpackPlugin({
-          inject: true,
-          template: `./${page}.html`,
-          filename: `${page}.html`,
-          chunks: [page],
-        })
-    )
-  ),
-};
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: `./${page}.html`,
+        filename: `${page}.html`,
+        chunks: [page],
+      }),
+    ],
+  };
+});
